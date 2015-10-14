4f25cd862c13aa2e5a63ca3c3026e38364dacbe0
Gilmar Pupo <g@g1mr.com>
2015-10-14 16:51:19 -0300

    Payment namespace

---
+++ tests/TestCaseAbstract.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * This file is part of gpupo/adyen-sdk
+ *
+ * (c) Gilmar Pupo <g@g1mr.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * For more information, see
+ * <http://www.g1mr.com/adyen-sdk/>.
+ */
+
+namespace Gpupo\Tests\AdyenSdk;
+
+use Gpupo\AdyenSdk\Factory;
+use Gpupo\Tests\CommonSdk\TestCaseAbstract as CommonSdkTestCaseAbstract;
+
+abstract class TestCaseAbstract extends CommonSdkTestCaseAbstract
+{
+    private $factory;
+
+    public static function getResourcesPath()
+    {
+        return dirname(dirname(__FILE__)) . '/Resources/';
+    }
+
+    public function factoryClient()
+    {
+        return $this->getFactory()->getClient();
+    }
+
+    protected function getOptions()
+    {
+        return [
+        ];
+    }
+
+    protected function getFactory()
+    {
+        if (!$this->factory) {
+            $this->factory = Factory::getInstance()->setup($this->getOptions(), $this->getLogger());
+        }
+
+        return $this->factory;
+    }
+}
