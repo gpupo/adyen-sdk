f71f9fb782681f65be95dae32917b1ac569ef57e
Gilmar Pupo <g@g1mr.com>
2015-10-20 11:06:47 -0200

    Decorators

--- src/Payment/Request/Request.php
+++ src/Payment/Request/Request.php
@@ -16,15 +16,44 @@ namespace Gpupo\AdyenSdk\Payment\Request;
 
 use Gpupo\CommonSdk\Entity\EntityAbstract;
 use Gpupo\CommonSdk\Entity\EntityInterface;
+use Gpupo\AdyenSdk\Payment\Request\Decorator\CreditCardDecorator;
+use Gpupo\AdyenSdk\Payment\Request\Decorator\BoletoDecorator;
 
 class Request extends EntityAbstract implements EntityInterface
 {
     public function getSchema()
     {
         return [
-            "order"         => "object",
-            "type"          => "string",
-            'encryptedData' => 'string',
+            'order'             => 'object',
+            'type'              => 'string',
+            'encryptedData'     => 'string',
+            'reference'         => 'string',
+            'merchantAccount'   => 'string',
         ];
+    }
+
+    protected function resolveDecorator()
+    {
+        $dict = [
+            'credit-cart' => 'CreditCardDecorator',
+            'boleto'      => 'BoletoDecorator',
+        ];
+
+        $type = strtolower($this->getType());
+
+        if (!array_key_exists($type, $dict)) {
+            throw new \InvalidArgumentException('Request type ['.$type.']not exist!');
+        }
+
+        return $dict[$type];
+    }
+
+    public function toJson()
+    {
+        $decorator = $this->resolveDecorator();
+        $instance = new $decorator();
+        $instance->setRequest($this);
+
+        return $instance->toJson();
     }
 }
