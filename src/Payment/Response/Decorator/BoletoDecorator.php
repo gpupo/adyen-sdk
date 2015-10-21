65285cf82966aee635b5aa061a87477d2e2ffb71
Gilmar Pupo <g@g1mr.com>
2015-10-21 17:42:35 -0200

    Console requisitando boleto com sucesso

---
+++ src/Payment/Response/Decorator/BoletoDecorator.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * This file is part of gpupo/adyen-sdk
+ *
+ * (c) Gilmar Pupo <g@g1mr.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * For more information, see
+ * <http://www.g1mr.com/adyen-sdk/>.
+ */
+
+namespace Gpupo\AdyenSdk\Payment\Response\Decorator;
+
+class BoletoDecorator extends AbstractDecorator
+{
+    protected function getBoletoFields()
+    {
+        return [
+            'barCodeReference',
+            'data',
+            'dueDate',
+            'url',
+            'expirationDate',
+        ];
+    }
+
+    public function getSchema()
+    {
+        $list = parent::getSchema();
+
+        foreach ($this->getBoletoFields() as $item) {
+            $list[$item] = 'string';
+        }
+
+        return $list;
+    }
+
+    protected function beforeConstruct($data = null)
+    {
+        if (array_key_exists('additionalData', $data) && is_array($data['additionalData'])) {
+            foreach ($this->getBoletoFields() as $item) {
+                $key = 'boletobancario.' . $item;
+                if (array_key_exists($key, $data['additionalData'])) {
+                    $data[$item] = $data['additionalData'][$key];
+                    unset($data['additionalData'][$key]);
+                }
+            }
+        }
+
+        return $data;
+    }
+
+}
