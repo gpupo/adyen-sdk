ece9c70dc288aec93b9dce0879b940b17857bbc0
Gilmar Pupo <g@g1mr.com>
2015-11-12 17:24:19 -0200

    doc

--- src/Payment/Request/Request.php
+++ src/Payment/Request/Request.php
@@ -18,6 +18,18 @@ use Gpupo\AdyenSdk\Factory;
 use Gpupo\CommonSdk\Entity\EntityAbstract;
 use Gpupo\CommonSdk\Entity\EntityInterface;
 
+/**
+ *
+ * @method Gpupo\AdyenSdk\Payment\Request\Order\Order getOrder()    Acesso a order
+ * @method setOrder(Gpupo\AdyenSdk\Payment\Request\Order\Order $order)    Define order
+ * @method string getType()    Acesso a type
+ * @method setType(string $type)    Define type
+ * @method string getEncryptedData()    Acesso a encryptedData
+ * @method setEncryptedData(string $encryptedData)    Define encryptedData
+ * @method string getMerchantAccount()    Acesso a merchantAccount
+ * @method setMerchantAccount(string $merchantAccount)    Define merchantAccount
+ *
+ */
 class Request extends EntityAbstract implements EntityInterface
 {
     public function getSchema()
